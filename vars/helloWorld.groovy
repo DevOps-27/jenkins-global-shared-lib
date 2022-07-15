616e521fa442fa9543b22c1a21fa6f8cc47c6d3e
Fajisola <99806550+Fajisola@users.noreply.github.com>
2022-07-14 19:47:03 -0600

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,13 +3,18 @@
 } */
 
 
-/* def call(Map config = [:]) {
+ def call(Map config = [:]) {  // [:] means empty map
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-} */
+} 
+// this is a better option than the one below
 
-def call(String name, String dayOfWeek) {
+
+//            OR
+
+/*def call(String name, String dayOfWeek) {
   sh "echo Hello ${name}. Today is ${dayOfWeek}"
   // you will need to modify the pipleline script to show the value for name annd dayOfWeek
   // the script will look like this below
   // helloWorld("Veronica", "Thursday")
-}
+  // this is not the best option because the value of the string variable could be easily mistakenly reversed. e.g hello Thursday. Today is Veronica
+} */
